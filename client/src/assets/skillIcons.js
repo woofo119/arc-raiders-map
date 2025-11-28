0a285a61cbe9e4bfeec2258ff159d537cb2bd773
woofo119 <dkdk4789@naver.com>
2025-11-29 03:23:50 +0900

    Fix: Move skill icons to assets and use imports to resolve path issues

---
+++ client/src/assets/skillIcons.js
@@ -0,0 +1,53 @@
+import c1 from './sk/imgi_30_used_to_the_weight.png';
+import c2 from './sk/imgi_32_gentle_pressure.png';
+import c3 from './sk/imgi_34_proficient_pryer.png';
+import c5 from './sk/imgi_31_blast_born.png';
+import c6 from './sk/imgi_33_fight_or_flight.png';
+import c8 from './sk/imgi_39_effortless_swing.png';
+import c9 from './sk/imgi_42_sky_clearing_swing.png';
+import c10 from './sk/imgi_37_a_little_extra.png';
+import c11 from './sk/imgi_40_loaded_arms.png';
+import c12 from './sk/imgi_38_downed_but_determined.png';
+import c13 from './sk/imgi_41_turtle_crawl.png';
+import c4 from './sk/imgi_36_unburdened_roll.png';
+import c7 from './sk/imgi_35_survivors_stamina.png';
+import c14 from './sk/imgi_44_flyswatter.png';
+import c15 from './sk/imgi_43_back_on_your_feet.png';
+
+import m1 from './sk/imgi_51_calming_stroll.png';
+import m2 from './sk/imgi_48_youthful_lungs.png';
+import m3 from './sk/imgi_55_vigorous_vaulter.png';
+import m5 from './sk/imgi_45_nimble_climber.png';
+import m6 from './sk/imgi_57_ready_to_roll.png';
+import m9 from './sk/imgi_50_carry_the_momentum.png';
+import m10 from './sk/imgi_56_heroic_leap.png';
+import m11 from './sk/imgi_49_sturdy_ankles.png';
+import m12 from './sk/imgi_54_off_the_wall.png';
+import m13 from './sk/imgi_47_slip_and_slide.png';
+import m4 from './sk/imgi_46_marathon_runner.png';
+import m7 from './sk/imgi_53_effortless_roll.png';
+import m14 from './sk/imgi_52_crawl_before_you_walk.png';
+import m15 from './sk/imgi_59_vault_spring.png';
+import m16 from './sk/imgi_58_vaults_on_vaults_on_vaults.png';
+
+import s1 from './sk/imgi_66_good_as_new.png';
+import s2 from './sk/imgi_69_stubborn_mule.png';
+import s3 from './sk/imgi_61_looters_instincts.png';
+import s5 from './sk/imgi_65_suffer_in_silence.png';
+import s6 from './sk/imgi_62_revitalizing_squat.png';
+import s8 from './sk/imgi_64_in_round_crafting.png';
+import s9 from './sk/imgi_72_three_deep_breaths.png';
+import s10 from './sk/imgi_67_traveling_tinkerer.png';
+import s11 from './sk/imgi_70_one_raiders_scraps.png';
+import s12 from './sk/imgi_68_broad_shoulders.png';
+import s13 from './sk/imgi_71_looters_luck.png';
+import s4 from './sk/imgi_63_silent_scavenger.png';
+import s14 from './sk/imgi_74_minesweeper.png';
+import s15 from './sk/imgi_73_security_breach.png';
+import s16 from './sk/imgi_60_agile_croucher.png';
+
+export const skillIcons = {
+    c1, c2, c3, c5, c6, c8, c9, c10, c11, c12, c13, c4, c7, c14, c15,
+    m1, m2, m3, m5, m6, m9, m10, m11, m12, m13, m4, m7, m14, m15, m16,
+    s1, s2, s3, s5, s6, s8, s9, s10, s11, s12, s13, s4, s14, s15, s16
+};
